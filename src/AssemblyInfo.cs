8dd2d91714b3dff105660f5bcb0ff39965267737
remi <remi@remitaylor.com>
2010-12-08 23:46:10 -0700

    Standard Layout

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("0.1.0.0")]
+
+[assembly: AssemblyTitle("Standard Library for OWIN")]
+[assembly: AssemblyProduct("OWIN")]
+[assembly: AssemblyDescription("Standard Library for OWIN")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © 2010 remitaylor")]
