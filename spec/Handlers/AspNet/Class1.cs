ef22b6f6e84e86c696045758dc45ebe0326746fd
remi <remi@remitaylor.com>
2010-12-12 00:44:04 -0700

    stubbed Owin.Handlers.AspNet

---
+++ spec/Handlers/AspNet/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Owin.Handlers.AspNet.Specs {
+
+    public class Class1 {
+    }
+}
---
+++ src/Handlers/AspNet/AspNet.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Owin.Handlers {
+
+    public class AspNet {
+    }
+}
